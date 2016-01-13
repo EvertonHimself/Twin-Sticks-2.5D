ab8e1eaea0f3ca28436cec762a7e10addd5fddc8
EvertonHimself <evertondeoliveira@gmail.com>
2016-01-13 19:10:33 -0200

    Using CrossPlatformInputManager.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+/// <summary>
+/// The Player class.
+/// </summary>
+public class Player : MonoBehaviour 
+{
+	void Update()
+	{
+		Debug.Log ("H: " + CrossPlatformInputManager.GetAxis ("Horizontal"));		
+		Debug.Log ("V: " + CrossPlatformInputManager.GetAxis ("Vertical"));		
+	}
+}
